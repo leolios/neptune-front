7c985c3e170b0447137217dc4d4ea57d43ea16cd
6346563751* <b0bb1e@protonmail.com>
2021-01-24 19:16:25 +0100

    app

---
+++ pages/app.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div class="sidebar-item">
+      <p>{{user.username}}</p>
+      <img v-bind:src="user.avatar" alt="">
+    </div>
+    <div class="main-item">
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {}
+    }
+  },
+  async fetch() {
+    const headers = new Headers();
+    headers.append("token", localStorage.getItem("token"));
+    this.user = await fetch(
+      'https://neptuneapp-api.herokuapp.com/api/token_info',
+      {
+        headers: headers
+      }
+    ).then(res => res.json());
+    console.log(this.user);
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+}
+
+.container > .sidebar-item {
+  flex: 1;
+  height: 100vh;
+  background-color: red;
+}
+
+.container > .main-item {
+	flex: 14;
+  height: 100vh;
+  background-color: blue;
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -17,7 +17,6 @@
 </template>
 
 <script>
-  //  @click="register"
   export default {
     data() {
       return {
