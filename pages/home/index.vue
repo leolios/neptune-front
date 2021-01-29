90e26af3672624816f5857b25c0c95dc1c313bb0
6346563751* <b0bb1e@protonmail.com>
2021-01-29 15:41:07 +0100

    organization in folder

---
+++ pages/home/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="first">
+    <div class="void-main">
+      <h1 class="h1-main">
+        NEPTUNE
+      </h1>
+    </div>
+    <div id="appear-2" class="appear-2">
+      <div class="full">
+        <div class="navbar">
+          <ul>
+            <li class="join">  <NuxtLink to="/register">INSCRIPTION</NuxtLink></li>
+          </ul>
+          <ul>
+            <li class="li-navbar"><a href="/login">SE CONNECTER</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="void-secondary">
+    </div>
+    <div id="appear-1" class="appear-1">
+      <div class="full">
+        <h1 class="full-h1">
+          Vous aimez les gens et bien nous aussi
+        </h1>
+      </div>
+    </div>
+    <div class="void-secondary">
+    </div>
+    <div id="appear-2" class="appear-2">
+      <div class="full">
+        <h1 class="full-h1">
+          Vous avez la tête dans les étoiles, nous dans les planétes
+        </h1>
+      </div>
+    </div>
+    <div class="void-before-footer">
+      <a class="join" href="/register">Rejoignez-nous</a>
+    </div>
+    <div class="footer">
+      <hr class="hr-border">
+      <div>
+        <ul>
+          <li class="footer-plan">Les pages du sites</li>
+          <li><a class="footer-link" href="/">Home </a></li>
+          <li><a class="footer-link btn-request" href="/formulaire" target="_blank">Formulaire de Recrutement</a></li>
+          <li><a class="footer-link btn-request" href="/404">Erreur</a></li>
+
+        </ul>
+        <ul>
+          <li class="footer-separation-li"></li>
+        </ul>
+        <ul>
+          <li class="footer-plan">Les sites</li>
+          <li><a class="footer-link" href="https://discord.com" target="_blank">Discord ( le concurrent lol )</a></li>
+          <li><a class="footer-link btn-request" href="https://facebook.com" target="_blank">Pareil que nous mais pour les vieux</a></li>
+        </ul>
+      </div>
+      <hr class="hr-border">
+      <Copyright>Neptune 2020 © tous droits réservés</Copyright>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+  })
+</script>
+
+<style src="./index.css"></style>
