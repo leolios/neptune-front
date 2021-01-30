9afd0656c5dd810eb0792c1478775984e047bea7
6346563751* <b0bb1e@protonmail.com>
2021-01-30 15:03:26 +0100

    ajout guild

--- pages/app/app.js
+++ pages/app/app.js
@@ -1,7 +1,9 @@
 export default {
   data() {
     return {
-      user: {}
+      user: {},
+      message: undefined,
+      guilds: []
     }
   },
   async fetch() {
@@ -13,7 +15,12 @@ export default {
         headers: headers
       }
     ).then(res => res.json());
-    console.log(this.user);
+    this.user.guilds.forEach(async (item, i) => {
+      let guild = await fetch('https://neptuneapp-api.herokuapp.com/api/guild/'+item).then(res => res.json());
+      this.guilds.push(guild.guild[0]);
+      console.log(this.guilds)
+    });
+
   },
   methods: {
     open_modal() {
@@ -28,8 +35,23 @@ export default {
       var modal = document.getElementById("modal_add_server");
       modal.style.display = "none";
     },
-    create_guild() {
-      
+    async create_guild() {
+      const form = new URLSearchParams();
+      form.append("name", document.getElementById("name").value)
+      form.append("description", document.getElementById("description").value)
+      form.append("icon", document.getElementById("icon").value)
+      form.append("region", document.getElementById("region").value)
+      form.append("token", localStorage.getItem('token'))
+
+      const req = await fetch("https://neptuneapp-api.herokuapp.com/guild", {
+        method: "POST",
+        headers: form
+      })
+      const json = await req.json();
+      if (json.message) this.message = json.message;
+      let guild = await fetch('https://neptuneapp-api.herokuapp.com/api/guild/'+json.new_server[0].id).then(res => res.json());
+      this.guilds.push(guild.guild[0]);
+      // this.$router.push({ path: 'app' })
     }
   }
 }
--- pages/app/index.vue
+++ pages/app/index.vue
@@ -5,12 +5,13 @@
         <span class="tooltiptext">{{user.username}}</span>
       </div>
       <div class="separator"></div>
-      <div v-for="guild of user.guilds" class="item"><img v-bind:src="user.avatar" alt="">
-        <span class="tooltiptext">{{user.username}}</span>
+      <div v-for="guild of guilds" class="item"><img v-bind:src="guild.icon" alt="">
+        <span class="tooltiptext">{{guild.name}}</span>
       </div>
       <div class="item" v-on:click="open_modal()">
         <div class="plus">
-<svg height="100%" viewBox="0 0 469.33333 469.33333" xmlns="http://www.w3.org/2000/svg"><path d="m437.332031 192h-160v-160c0-17.664062-14.335937-32-32-32h-21.332031c-17.664062 0-32 14.335938-32 32v160h-160c-17.664062 0-32 14.335938-32 32v21.332031c0 17.664063 14.335938 32 32 32h160v160c0 17.664063 14.335938 32 32 32h21.332031c17.664063 0 32-14.335937 32-32v-160h160c17.664063 0 32-14.335937 32-32v-21.332031c0-17.664062-14.335937-32-32-32zm0 0"/></svg>
+            <svg height="100%" viewBox="0 0 469.33333 469.33333" xmlns="http://www.w3.org/2000/svg">
+            <path d="m437.332031 192h-160v-160c0-17.664062-14.335937-32-32-32h-21.332031c-17.664062 0-32 14.335938-32 32v160h-160c-17.664062 0-32 14.335938-32 32v21.332031c0 17.664063 14.335938 32 32 32h160v160c0 17.664063 14.335938 32 32 32h21.332031c17.664063 0 32-14.335937 32-32v-160h160c17.664063 0 32-14.335937 32-32v-21.332031c0-17.664062-14.335937-32-32-32zm0 0"/></svg>
         </div>
         <span class="tooltiptext">Ajouter un serveur</span>
       </div>
@@ -19,29 +20,17 @@
       <p></p>
     </div>
     <div id="modal_add_server" class="modal" v-on:click="close_modal($event)">
-
-      <!-- Modal content -->
       <div class="modal-content">
-        <!--<span class="close" v-on:click="close_modal($event)">&times;</span>
-        <h2>Créer un serveur</h2>
-        <input type="text" id="fname" name="fname" placeholder="Nom du serveur">
-        <textarea type="text" id="fname" name="fname" placeholder="Description">
-        </textarea>
-        <input type="text" id="fname" name="fname" placeholder="Icon">
-        <select name="region">
-          <option value="eu" selected>Eu</option>
-          <option value="us">Us</option>
-      </select>-->
       <div class="login-form">
         <p class="login-text">
           Créer un serveur
           <span class="fa-stack fa-lg">
           </span>
         </p>
-        <input type="text" name="email" class="login-username" autofocus="true" required="true" placeholder="Nom du serveur" />
-        <textarea type="text" name="password" class="login-username" required="true" placeholder="Description" />
-        <input type="text" name="password" class="login-username" required="true" placeholder="Icon" />
-        <select name="region" class="login-username">
+        <input type="text" id="name" name="name" class="login-username" autofocus="true" required="true" placeholder="Nom du serveur" />
+        <textarea type="text" id="description" name="description" class="login-username" required="true" placeholder="Description" />
+        <input type="text" id="icon" name="icon" class="login-username" required="true" placeholder="Icon" />
+        <select name="region" id="region" class="login-username">
           <option value="eu" selected>Eu</option>
           <option value="us">Us</option>
         </select
