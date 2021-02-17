1c78986a201f6f19427fa28fc04217e1ad445e67
Bobbie <b0bb1e@protonmail.com>
2021-02-17 23:07:39 +0100

    add title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'neptune-front',
+    title: 'Neptune - The encrypted social network',
     htmlAttrs: {
       lang: 'en'
     },
