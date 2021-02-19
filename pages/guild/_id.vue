45b1f0a652a23ec673e2b08d52c22be8abb961e7
Bobbie <b0bb1e@protonmail.com>
2021-02-19 17:37:21 +0100

    feature: page guild/:id

---
+++ pages/guild/_id.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div>
+    <ChannelList></ChannelList>
+  </div>
+</template>
+
+<script>
+export default {
+  layout (context) {
+    return 'app'
+  },
+  name: 'guild',
+  data () {
+      return {
+          id: 0,
+          msg: 'Hey Nic Raboy'
+      }
+  },
+  async created() {
+      this.id = this.$route.params.id
+      this.$store.commit("setActualGuild", this.id)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
